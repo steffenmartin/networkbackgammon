ecb38f492ea2ac8f514ce85dd9f4a91a85f3d4b2
steffen.martin.jena <steffen.martin.jena@b2694f1c-16b8-11df-9efd-8136817ac53b>
2010-02-27 08:10:32 +0000

    Spike project to see whether bi-directional .NET remoting is possible.

---
+++ networkbackgammon --username Paulr.Silva/RemotingBiDirectionalSpike/IRemotingServer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RemotingBiDirectionalSpike
+{
+    public interface IRemotingServer
+    {
+        void SendMessage(string _message);
+
+        void RegisterMessageCallback(RemotingClient.ClientCallback callback);
+        void UnregisterMessageCallback(RemotingClient.ClientCallback callback);
+    }
+}
---
+++ networkbackgammon --username Paulr.Silva/RemotingBiDirectionalSpike/RemotingServer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RemotingBiDirectionalSpike
+{
+    public class RemotingServer : MarshalByRefObject, IRemotingServer
+    {
+        #region IRemotingServer Members
+
+        public event RemotingClient.ClientCallback messageCallback;
+
+        public void SendMessage(string _message)
+        {
+            messageCallback(_message);
+
+            if (MainWindow.mainWindow != null)
+            {
+                MainWindow.mainWindow.OnMessageReceivedOnServer(_message);
+            }
+        }
+
+        public void RegisterMessageCallback(RemotingClient.ClientCallback callback)
+        {
+            messageCallback += callback;
+        }
+
+        public void UnregisterMessageCallback(RemotingClient.ClientCallback callback)
+        {
+            messageCallback -= callback;
+        }
+
+        #endregion
+    }
+}
