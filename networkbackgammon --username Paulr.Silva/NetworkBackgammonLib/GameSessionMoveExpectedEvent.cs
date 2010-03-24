9c15616d194e7e4090ea5005c4d8ab9840a75b06
steffen.martin.jena <steffen.martin.jena@b2694f1c-16b8-11df-9efd-8136817ac53b>
2010-03-24 06:19:22 +0000

    Separated checker update and move expected event. Also added a further state to the state machine for the acknowledgment of the active player if no (valid) moves are possible.

---
+++ networkbackgammon --username Paulr.Silva/NetworkBackgammonLib/GameSessionMoveExpectedEvent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetworkBackgammonLib
+{
+    /// <summary>
+    /// Event for signaling players which one of them is epxected to make the next move
+    /// </summary>
+    [Serializable]
+    public class GameSessionMoveExpectedEvent : NetworkBackgammonGameSessionEvent
+    {
+        #region Members
+
+        /// <summary>
+        /// Active player (the one who's expected to make the next move)
+        /// </summary>
+        private string activePlayer;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="_activePlayer">Active player</param>
+        public GameSessionMoveExpectedEvent(string _activePlayer)
+        {
+            activePlayer = _activePlayer;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the active player (the one who's expected to make the next move)
+        /// </summary>
+        public string ActivePlayer
+        {
+            get
+            {
+                return activePlayer;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ networkbackgammon --username Paulr.Silva/NetworkBackgammonLib/GameSessionNoPossibleMovesAcknowledgeEvent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetworkBackgammonLib
+{
+    /// <summary>
+    /// Event for confirmation of the active players who has no (valid) moves
+    /// </summary>
+    [Serializable]
+    public class GameSessionNoPossibleMovesAcknowledgeEvent : NetworkBackgammonGameSessionEvent
+    {
+        #region Members
+
+        /// <summary>
+        /// Player who akcnowledges no (valid) moves
+        /// </summary>
+        private string playerName;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="_player">Player who akcnowledges no (valid) moves</param>
+        public GameSessionNoPossibleMovesAcknowledgeEvent(string _playerName)
+        {
+            playerName = _playerName;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the name of the player who akcnowledges no (valid) moves
+        /// </summary>
+        public string PlayerName
+        {
+            get
+            {
+                return playerName;
+            }
+        }
+
+        #endregion
+    }
+}
