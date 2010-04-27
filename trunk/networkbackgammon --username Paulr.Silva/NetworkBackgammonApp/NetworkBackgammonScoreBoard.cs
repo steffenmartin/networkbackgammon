7898ab99489f6a6ca77b3c1fce813de3df1cd040
paulr.silva <paulr.silva@b2694f1c-16b8-11df-9efd-8136817ac53b>
2010-04-27 03:47:10 +0000

    Update player pip count on the scoreboard

--- trunk/networkbackgammon --username Paulr.Silva/NetworkBackgammonApp/NetworkBackgammonScoreBoard.cs
+++ trunk/networkbackgammon --username Paulr.Silva/NetworkBackgammonApp/NetworkBackgammonScoreBoard.cs
@@ -14,7 +14,12 @@ namespace NetworkBackgammon
     public partial class NetworkBackgammonScoreBoard : Form, INetworkBackgammonListener
     {
         INetworkBackgammonListener m_defaultListener = new NetworkBackgammonListener();
-    
+
+        /// <summary>
+        /// Delegate for handling pip updates
+        /// </summary>
+        delegate void OnUpdatePipCountDelegate(Int32 pipCount);
+
         public NetworkBackgammonScoreBoard()
         {
             InitializeComponent();
@@ -34,10 +39,37 @@ namespace NetworkBackgammon
 
         public void OnEventNotification(INetworkBackgammonNotifier sender, INetworkBackgammonEvent e)
         {
-            // TODO: Handle events
+            if (e is GameSessionCheckerUpdatedEvent)
+            {
+                GameSessionCheckerUpdatedEvent updateEvent = (GameSessionCheckerUpdatedEvent)e;
+
+                NetworkBackgammonPlayer curPlayer = updateEvent.GetPlayerByName(Title);
+
+                if (curPlayer != null)
+                {
+                    // Update the pip count
+                    if (InvokeRequired)
+                    {
+                        BeginInvoke(new OnUpdatePipCountDelegate(OnUpdatePipCount), curPlayer.PipCount);
+                    }
+                    else
+                    {
+                        OnUpdatePipCount(curPlayer.PipCount);
+                    }
+                }
+            }
         }
 
         #endregion
+
+        /// <summary>
+        /// Handler for a checker/pip update
+        /// </summary>
+        private void OnUpdatePipCount(Int32 pipCount)
+        {
+            this.player1Pips.Text = pipCount.ToString();
+        }
+
 
         #region Properties
 
@@ -56,9 +88,33 @@ namespace NetworkBackgammon
 
         #endregion
 
-        private void NetworkBackgammonScoreBoard_Load(object sender, EventArgs e)
+        private void groupPlayerScoreBoard_VisibleChanged(object sender, EventArgs e)
         {
-            
+            if (Visible)
+            {
+                // Become a listener of the player 
+                if (NetworkBackgammonClient.Instance.Player != null)
+                {
+                    NetworkBackgammonClient.Instance.Player.AddListener(this);
+                }
+            }
+            else
+            {
+                // Remove self as a listener of player
+                if (NetworkBackgammonClient.Instance.Player != null)
+                {
+                    NetworkBackgammonClient.Instance.Player.RemoveListener(this);
+                }
+            }
+        }
+
+        private void NetworkBackgammonScoreBoard_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Remove self as a listener of player
+            if (NetworkBackgammonClient.Instance.Player != null)
+            {
+                NetworkBackgammonClient.Instance.Player.RemoveListener(this);
+            }
         }
     }
 }
