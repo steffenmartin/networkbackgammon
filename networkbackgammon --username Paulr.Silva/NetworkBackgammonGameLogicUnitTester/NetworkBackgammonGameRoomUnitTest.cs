0c0340fcc02dd84e1683724d1e9509a614d20f5c
steffen.martin.jena <steffen.martin.jena@b2694f1c-16b8-11df-9efd-8136817ac53b>
2010-05-05 05:36:43 +0000

    Added framework for testing the game room and implemented one pseudo-black-box test for the verification of connecting and disconnecting (i.e. registering and login/logout) of players similar to the simulator (i.e. not using remoting). Fixes issue #36.

---
+++ networkbackgammon --username Paulr.Silva/NetworkBackgammonGameLogicUnitTester/NetworkBackgammonGameRoomUnitTest.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetworkBackgammonRemotingLib;
+using NetworkBackgammonLib;
+
+namespace NetworkBackgammonGameLogicUnitTester
+{
+    /// <summary>
+    /// Various tests to verify functionality and validity of the game room
+    /// </summary>
+    [TestClass]
+    public class NetworkBackgammonGameRoomUnitTest
+    {
+        #region Members
+
+        /// <summary>
+        /// Test context
+        /// </summary>
+        private TestContext testContextInstance;
+
+        /// <summary>
+        /// The game room (under test)
+        /// </summary>
+        NetworkBackgammonRemoteGameRoom gameRoom = null;
+
+        /// <summary>
+        /// Controller for (simulating) player 1
+        /// </summary>
+        NetworkBackgammonPlayerController player1Controller = null;
+
+        /// <summary>
+        /// Controller for (simulating) player 2
+        /// </summary>
+        NetworkBackgammonPlayerController player2Controller = null;
+
+        #endregion
+
+        #region Methods
+
+        #region Test Methods
+
+        // Use TestInitialize to run code before running each test 
+        [TestInitialize()]
+        public void Initialize()
+        {
+            // Create game room
+            gameRoom = new NetworkBackgammonRemoteGameRoom();
+
+            Assert.IsNotNull(gameRoom, "Game room hasn't been created");
+
+            // Create player controllers
+            player1Controller = new NetworkBackgammonPlayerController();
+            player2Controller = new NetworkBackgammonPlayerController();
+
+            Assert.IsNotNull(player1Controller, "Player 1 controller hasn't been created");
+            Assert.IsNotNull(player2Controller, "Player 2 controller hasn't been created");
+
+            // Connect player controllers to game room
+            player1Controller.ConnectedGameRoom = gameRoom;
+            player2Controller.ConnectedGameRoom = gameRoom;
+        }
+
+        /// <summary>
+        /// Verifies whether 2 players can connect to and disconnect from a game room incl. the expected notifications
+        /// </summary>
+        [TestMethod]
+        public void TestMethod_GameRoomConnectDisconnect()
+        {
+            GameRoomConnectBothPlayers();
+
+            // Check the events received by player 1
+
+            // Player 1 should have received a game room event due to the connection of player 2
+            Assert.IsTrue(player1Controller.EventQueue.Count > 0, "Player 1 did not received an event");
+            KeyValuePair<INetworkBackgammonNotifier, INetworkBackgammonEvent> queueElement = player1Controller.EventQueue.Dequeue();
+            Assert.IsInstanceOfType(queueElement.Value, typeof(NetworkBackgammonGameRoomEvent), "Player 1 did not receive a game room event");
+            Assert.IsTrue(((NetworkBackgammonGameRoomEvent)(queueElement.Value)).EventType == NetworkBackgammonGameRoomEvent.GameRoomEventType.PlayerConnected, "Player 1 did not receive notification of connection of another player");
+
+            // Check the events received by player 2
+
+            // Player 2 should have received no game room event due to the connection of player 2
+            Assert.IsTrue(player2Controller.EventQueue.Count == 0, "Player 2 did received an event");
+
+            DisconnectPlayer1();
+
+            // Check the events received by player 2
+
+            // Player 2 should have received a game room event due to the disconnection of player 1
+            Assert.IsTrue(player2Controller.EventQueue.Count > 0, "Player 2 did not received an event");
+            queueElement = player2Controller.EventQueue.Dequeue();
+            Assert.IsInstanceOfType(queueElement.Value, typeof(NetworkBackgammonGameRoomEvent), "Player 2 did not receive a game room event");
+            Assert.IsTrue(((NetworkBackgammonGameRoomEvent)(queueElement.Value)).EventType == NetworkBackgammonGameRoomEvent.GameRoomEventType.PlayerDisconnected, "Player 2 did not receive notification of disconnection of another player");
+
+            // Check the events received by player 1
+
+            // Player 1 should have received no game room event due to the connection of player 1
+            Assert.IsTrue(player1Controller.EventQueue.Count == 0, "Player 1 did received an event");
+
+            DisconnectPlayer2();
+        }
+
+        #endregion
+
+        #region Utility Methods
+
+        private void GameRoomConnectBothPlayers()
+        {
+            ConnectPlayer1();
+
+            ConnectPlayer2();
+        }
+
+        private void ConnectPlayer1()
+        {
+            bool bRetVal;
+            string returnMessage = "";
+
+            // Connect player 1 to game room
+            bRetVal = player1Controller.OnConnect("Player1", "Player1", ref returnMessage);
+            Assert.IsTrue(bRetVal, returnMessage);
+        }
+
+        private void ConnectPlayer2()
+        {
+            bool bRetVal;
+            string returnMessage = "";
+
+            // Connect player 2 to game room
+            bRetVal = player2Controller.OnConnect("Player2", "Player2", ref returnMessage);
+            Assert.IsTrue(bRetVal, returnMessage);
+        }
+
+        private void GameRoomDisconnectBothPlayers()
+        {
+            DisconnectPlayer1();
+
+            DisconnectPlayer2();
+        }
+
+        private void DisconnectPlayer1()
+        {
+            bool bRetVal;
+            string returnMessage = "";
+
+            // Disconnect player 1 from game room
+            bRetVal = player1Controller.OnDisconnect(ref returnMessage);
+            Assert.IsTrue(bRetVal, returnMessage);
+        }
+
+        private void DisconnectPlayer2()
+        {
+            bool bRetVal;
+            string returnMessage = "";
+
+            // Disconnect player 2 from game room
+            bRetVal = player2Controller.OnDisconnect(ref returnMessage);
+            Assert.IsTrue(bRetVal, returnMessage);
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ networkbackgammon --username Paulr.Silva/NetworkBackgammonGameLogicUnitTester/NetworkBackgammonPlayerController.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NetworkBackgammonRemotingLib;
+using NetworkBackgammonLib;
+
+namespace NetworkBackgammonGameLogicUnitTester
+{
+    /// <summary>
+    /// Test utility class to control (simulate) the behavior of a player
+    /// </summary>
+    public class NetworkBackgammonPlayerController : INetworkBackgammonListener
+    {
+        #region Members
+
+        /// <summary>
+        /// The game room this player is (going to be) connected to
+        /// </summary>
+        private NetworkBackgammonRemoteGameRoom gameRoom = null;
+
+        /// <summary>
+        /// The player object as returned by the game room on connect
+        /// </summary>
+        private NetworkBackgammonPlayer player = null;
+
+        /// <summary>
+        /// Default network backgammon listener
+        /// </summary>
+        INetworkBackgammonListener defaultListener = new NetworkBackgammonListener();
+
+        /// <summary>
+        /// Queue for incoming events from the game room
+        /// </summary>
+        Queue<KeyValuePair<INetworkBackgammonNotifier, INetworkBackgammonEvent>> eventQueue = new Queue<KeyValuePair<INetworkBackgammonNotifier,INetworkBackgammonEvent>>();
+
+        #endregion
+
+        #region Methods
+
+        public bool OnConnect(string playerName, string playerPassword, ref string returnMessage)
+        {
+            bool bRetVal = false;
+
+            if (player == null)
+            {
+                if (gameRoom != null)
+                {
+                    // Register (if not already; ignore 'false' return value if already registered)
+                    gameRoom.RegisterPlayer(playerName, playerPassword);
+                    
+                    // Enter the game room
+                    player = gameRoom.Enter(playerName, playerPassword);
+
+                    if (player != null)
+                    {
+                        player.AddListener(this);
+
+                        bRetVal = true;
+                    }
+                    else
+                    {
+                        if (returnMessage != null)
+                        {
+                            returnMessage += "\nPlayer couldn't login with name '" + playerName + "' and password '" + playerPassword + "'.";
+                        }
+                    }
+                }
+                else
+                {
+                    if (returnMessage != null)
+                    {
+                        returnMessage += "\nNo game room available to connect to!";
+                    }
+                }
+            }
+            else
+            {
+                if (returnMessage != null)
+                {
+                    returnMessage += "\nPlayer with name '" + playerName + "' is already connected!";
+                }
+
+                bRetVal = false;
+            }
+
+            return bRetVal;
+        }
+
+        public bool OnDisconnect(ref string returnMessage)
+        {
+            bool bRetVal = false;
+
+            if (player != null)
+            {
+                if (gameRoom != null)
+                {
+                    gameRoom.Leave(player);
+
+                    gameRoom.RemoveListener(this);
+                    player.RemoveListener(this);
+
+                    player = null;
+
+                    bRetVal = true;
+                }
+                else
+                {
+                    if (returnMessage != null)
+                    {
+                        returnMessage += "\nNo game room available to disconnect from!";
+                    }
+                }
+            }
+            else
+            {
+                if (returnMessage != null)
+                {
+                    returnMessage += "\nPlayer is not connected!";
+                }
+
+                bRetVal = false;
+            }
+
+            return bRetVal;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Sets or gets the game room that this player is connected to
+        /// </summary>
+        public NetworkBackgammonRemoteGameRoom ConnectedGameRoom
+        {
+            set
+            {
+                gameRoom = value;
+            }
+            get
+            {
+                return gameRoom;
+            }
+        }
+
+        /// <summary>
+        /// Checks whether or not the player is actually connected to a game room
+        /// </summary>
+        public bool IsConnected
+        {
+            get
+            {
+                return player != null;
+            }
+        }
+
+        public Queue<KeyValuePair<INetworkBackgammonNotifier, INetworkBackgammonEvent>> EventQueue
+        {
+            get
+            {
+                return eventQueue;
+            }
+        }
+
+        #endregion
+
+        #region INetworkBackgammonListener Members
+
+        public bool AddNotifier(INetworkBackgammonNotifier notifier)
+        {
+            if (notifier != null && defaultListener != null)
+            {
+                return defaultListener.AddNotifier(notifier);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool RemoveNotifier(INetworkBackgammonNotifier notifier)
+        {
+            if (notifier != null && defaultListener != null)
+            {
+                return defaultListener.RemoveNotifier(notifier);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void OnEventNotification(INetworkBackgammonNotifier sender, INetworkBackgammonEvent e)
+        {
+            eventQueue.Enqueue(new KeyValuePair<INetworkBackgammonNotifier, INetworkBackgammonEvent>(sender, e)); 
+        }
+
+        #endregion
+    }
+}
