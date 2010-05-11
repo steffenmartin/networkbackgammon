3eb360c132755e88a8ae9b7e0cd5f10def8f2dd8
steffen.martin.jena <steffen.martin.jena@b2694f1c-16b8-11df-9efd-8136817ac53b>
2010-05-11 14:49:06 +0000

    Added spike project to figure out how to use (register) multiple HTTP channels in one process (required for unit testing).

---
+++ trunk/networkbackgammon --username Paulr.Silva/MultipleChannelsSpike/MainWindow.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace MultipleChannelsSpike
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonChannel1 = new System.Windows.Forms.Button();
+            this.buttonChannel2 = new System.Windows.Forms.Button();
+            this.richTextBoxLogging = new System.Windows.Forms.RichTextBox();
+            this.textBoxChannel1Name = new System.Windows.Forms.TextBox();
+            this.textBoxChannel2Name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonChannel1
+            // 
+            this.buttonChannel1.Location = new System.Drawing.Point(207, 12);
+            this.buttonChannel1.Name = "buttonChannel1";
+            this.buttonChannel1.Size = new System.Drawing.Size(75, 23);
+            this.buttonChannel1.TabIndex = 0;
+            this.buttonChannel1.Text = "Register";
+            this.buttonChannel1.UseVisualStyleBackColor = true;
+            this.buttonChannel1.Click += new System.EventHandler(this.buttonChannel1_Click);
+            // 
+            // buttonChannel2
+            // 
+            this.buttonChannel2.Location = new System.Drawing.Point(205, 41);
+            this.buttonChannel2.Name = "buttonChannel2";
+            this.buttonChannel2.Size = new System.Drawing.Size(75, 23);
+            this.buttonChannel2.TabIndex = 1;
+            this.buttonChannel2.Text = "Register";
+            this.buttonChannel2.UseVisualStyleBackColor = true;
+            this.buttonChannel2.Click += new System.EventHandler(this.buttonChannel2_Click);
+            // 
+            // richTextBoxLogging
+            // 
+            this.richTextBoxLogging.Location = new System.Drawing.Point(12, 70);
+            this.richTextBoxLogging.Name = "richTextBoxLogging";
+            this.richTextBoxLogging.ReadOnly = true;
+            this.richTextBoxLogging.Size = new System.Drawing.Size(268, 191);
+            this.richTextBoxLogging.TabIndex = 2;
+            this.richTextBoxLogging.Text = "";
+            // 
+            // textBoxChannel1Name
+            // 
+            this.textBoxChannel1Name.Location = new System.Drawing.Point(58, 14);
+            this.textBoxChannel1Name.Name = "textBoxChannel1Name";
+            this.textBoxChannel1Name.Size = new System.Drawing.Size(143, 20);
+            this.textBoxChannel1Name.TabIndex = 3;
+            this.textBoxChannel1Name.Text = "Channel1";
+            // 
+            // textBoxChannel2Name
+            // 
+            this.textBoxChannel2Name.Location = new System.Drawing.Point(58, 43);
+            this.textBoxChannel2Name.Name = "textBoxChannel2Name";
+            this.textBoxChannel2Name.Size = new System.Drawing.Size(143, 20);
+            this.textBoxChannel2Name.TabIndex = 3;
+            this.textBoxChannel2Name.Text = "Channel2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Name:";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxChannel2Name);
+            this.Controls.Add(this.textBoxChannel1Name);
+            this.Controls.Add(this.richTextBoxLogging);
+            this.Controls.Add(this.buttonChannel2);
+            this.Controls.Add(this.buttonChannel1);
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.Text = "Multiple Channels Spike";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonChannel1;
+        private System.Windows.Forms.Button buttonChannel2;
+        private System.Windows.Forms.RichTextBox richTextBoxLogging;
+        private System.Windows.Forms.TextBox textBoxChannel1Name;
+        private System.Windows.Forms.TextBox textBoxChannel2Name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ trunk/networkbackgammon --username Paulr.Silva/MultipleChannelsSpike/MainWindow.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Http;
+using System.Collections;
+
+namespace MultipleChannelsSpike
+{
+    public partial class MainWindow : Form
+    {
+        private int channel1Port = 1024;
+        private HttpChannel channel1;
+        private int channel2Port = 1025;
+        private HttpChannel channel2;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonChannel1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                BinaryServerFormatterSinkProvider serverProv = new BinaryServerFormatterSinkProvider();
+                serverProv.TypeFilterLevel = System.Runtime.Serialization.Formatters.TypeFilterLevel.Full;
+                BinaryClientFormatterSinkProvider clientProv = new BinaryClientFormatterSinkProvider();
+
+                IDictionary props = new Hashtable();
+                props["port"] = channel1Port;
+                props["machineName"] = System.Environment.MachineName;
+                props["name"] = textBoxChannel1Name.Text;
+
+                // Register a client channel so the server an communicate back - it needs a channel
+                // opened for the callback to the CallbackSink object that is anchored on the client!
+                // channel = new HttpChannel(clientChannel++);
+                channel1 = new HttpChannel(props, clientProv, serverProv);
+
+                ChannelServices.RegisterChannel(channel1, false);
+
+                richTextBoxLogging.Text += "\nChannel with name '" + channel1.ChannelName + "' successfully registered";
+            }
+            catch (Exception ex)
+            {
+                richTextBoxLogging.Text += "\n" + ex.Message;
+            }
+        }
+
+        private void buttonChannel2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                BinaryServerFormatterSinkProvider serverProv = new BinaryServerFormatterSinkProvider();
+                serverProv.TypeFilterLevel = System.Runtime.Serialization.Formatters.TypeFilterLevel.Full;
+                BinaryClientFormatterSinkProvider clientProv = new BinaryClientFormatterSinkProvider();
+
+                IDictionary props = new Hashtable();
+                props["port"] = channel2Port;
+                props["machineName"] = System.Environment.MachineName;
+                props["name"] = textBoxChannel2Name.Text;
+
+                // Register a client channel so the server an communicate back - it needs a channel
+                // opened for the callback to the CallbackSink object that is anchored on the client!
+                // channel = new HttpChannel(clientChannel++);
+                channel2 = new HttpChannel(props, clientProv, serverProv);
+
+                ChannelServices.RegisterChannel(channel2, false);
+
+                richTextBoxLogging.Text += "\nChannel with name '" + channel2.ChannelName + "' successfully registered";
+            }
+            catch (Exception ex)
+            {
+                richTextBoxLogging.Text += "\n" + ex.Message;
+            }
+        }
+    }
+}
