3c0ed0b1ddac72c3e79afb51f027f2720f391dff
ComputerEyes1nm <ComputerEyes1nm@b2694f1c-16b8-11df-9efd-8136817ac53b>
2010-05-11 21:32:15 +0000

    Missed files for unit testing of remoting connection

---
+++ trunk/networkbackgammon --username Paulr.Silva/NetworkBackgammonRemotingLib/NetworkBackgammonActivateServer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Http;
+using System.Net;
+using System.Net.Sockets;
+
+using NetworkBackgammonLib;
+using NetworkBackgammonRemotingLib;
+
+namespace NetworkBackgammonRemotingLib
+{
+    public class NetworkBackgammonActivateServer
+    {
+        // Channal the server is connected to
+        private HttpChannel m_channel = null;
+        // Local server object
+        private NetworkBackgammonRemoteGameRoom gameRoom = null;
+        /**
+        *   Activate/Deactivate server
+        */
+        
+        public NetworkBackgammonRemoteGameRoom ActivateServer(string port)
+        {
+
+            if (m_channel == null)
+            {
+                // We need to use binary formatters, which allow the serialization of generic collections
+                BinaryServerFormatterSinkProvider serverProv = new BinaryServerFormatterSinkProvider();
+                serverProv.TypeFilterLevel = System.Runtime.Serialization.Formatters.TypeFilterLevel.Full;
+                BinaryClientFormatterSinkProvider clientProv = new BinaryClientFormatterSinkProvider();
+
+                IDictionary props = new Hashtable();
+                props["port"] = Convert.ToInt32(port);
+                props["name"] = String.Empty;
+
+                m_channel = new HttpChannel(props, clientProv, serverProv);
+            }
+            ChannelServices.RegisterChannel(m_channel, false);
+            RemotingConfiguration.RegisterWellKnownServiceType( typeof(NetworkBackgammonRemoteGameRoom), "GameRoom", WellKnownObjectMode.Singleton);
+
+            // Assign the instantiated remote server to the local server
+            MarshalByRefObject obj = (MarshalByRefObject)RemotingServices.Connect(typeof(NetworkBackgammonRemoteGameRoom), "http://127.0.0.1:" + port + "/GameRoom");
+            gameRoom = obj as NetworkBackgammonRemoteGameRoom;
+
+            return gameRoom;
+        }
+    }
+}
---
+++ trunk/networkbackgammon --username Paulr.Silva/NetworkBackgammonRemotingLib/NetworkBackgammonClient.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Http;
+using System.Net;
+using System.Net.Sockets;
+using NetworkBackgammonRemotingLib;
+using NetworkBackgammonLib;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace NetworkBackgammonRemotingLib
+{
+    public class NetworkBackgammonClient
+    {
+        static NetworkBackgammonClient instance=null;
+        // Lock to make the singleton thread sage
+        static readonly object padlock = new object();
+        // Remotable client object that will receive 
+        NetworkBackgammonPlayer player = null;
+        // Remote game room
+        NetworkBackgammonRemoteGameRoom gameRoom = null;
+        HttpChannel channel = null;
+        string serverIpAddress = "127.0.0.1";
+        string serverPort = "8080";
+
+        // Private Constructor
+        NetworkBackgammonClient()
+        {
+        }
+
+        // Access to the single instance of this class
+        public static NetworkBackgammonClient Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance==null)
+                    {
+                        instance = new NetworkBackgammonClient();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        // Remotable client object
+        public NetworkBackgammonPlayer Player
+        {
+            get
+            {
+                return player;
+            }
+
+            set
+            {
+                player = value;
+            }
+        }
+
+        // Remotable server object
+        public NetworkBackgammonRemoteGameRoom GameRoom
+        {
+            get
+            {
+                return gameRoom;
+            }
+        }
+
+        // Remotable server object
+        public bool IsConnected
+        {
+            get
+            {
+                return (gameRoom == null ? false : true);
+            }
+        }
+
+        // Server IP address property 
+        public string ServerIpAddress
+        {
+            get
+            {
+                return serverIpAddress;
+            }
+        }
+
+        // Server port property 
+        public string ServerPort
+        {
+            get
+            {
+                return serverPort;
+            }
+        }
+
+        // Send a message to the listeners of Player
+        public void SendMsg(string msg)
+        {
+            if( IsConnected )
+            {
+                NetworkBackgammonPlayer recpPlayer = GameRoom.GetOpposingPlayer(Player);
+
+                if( recpPlayer != null )
+                {
+                    Player.Broadcast( new NetworkBackgammonChatEvent(Player.PlayerName, recpPlayer.PlayerName, msg) );
+                }
+            }
+        }
+
+        // Connect to a remote server (remoting) via an ip address and port
+        public bool ConnectServer(string ipAddr, string port)
+        {
+            bool retval = false;
+
+            try
+            {
+                // First disconenct any previous connection with the server
+                DisconnectServer();
+                
+                // Get an available channel to use
+                int clientChannel = FindUnusedPort(IPAddress.Loopback);
+
+                if (clientChannel != 0)
+                {
+                    BinaryServerFormatterSinkProvider serverProv = new BinaryServerFormatterSinkProvider();
+                    serverProv.TypeFilterLevel = System.Runtime.Serialization.Formatters.TypeFilterLevel.Full;
+                    BinaryClientFormatterSinkProvider clientProv = new BinaryClientFormatterSinkProvider();
+
+                    IDictionary props = new Hashtable();
+                    props["port"] = clientChannel;
+                    props["machineName"] = System.Environment.MachineName;
+                    props["name"] = String.Empty;
+
+                    // Register a client channel so the server an communicate back - it needs a channel
+                    // opened for the callback to the CallbackSink object that is anchored on the client!
+                    // channel = new HttpChannel(clientChannel++);
+                    channel = new HttpChannel(props, clientProv, serverProv);
+                }
+                else
+                {
+                    throw new Exception("Couldn't find unused client port!");
+                }
+
+                // Registers a channel with the channel services
+                ChannelServices.RegisterChannel(channel, false);
+
+                // Now create a transparent proxy to the server component
+                MarshalByRefObject obj = (MarshalByRefObject)RemotingServices.Connect(typeof(NetworkBackgammonRemoteGameRoom), "http://" + ipAddr + ":" + port + "/GameRoom");
+                gameRoom = obj as NetworkBackgammonRemoteGameRoom;
+
+                serverIpAddress = ipAddr;
+                serverPort = port;
+
+                retval = gameRoom.VerifyConnection("ack") == "nack";
+            }
+            catch (Exception ex)
+            {
+                // Disconnect from the server
+                DisconnectServer();
+            }
+
+            return retval;
+        }
+
+        // Disconnect as from the server and remove client as listener
+        public void DisconnectServer()
+        {
+            if (IsConnected && (Player != null))
+            {
+                // Remove the player from the game room
+                gameRoom.Leave(Player);
+            }
+
+            if (channel != null)
+            {
+                ChannelDataStore cds = (ChannelDataStore)channel.ChannelData;
+
+                foreach (string s in cds.ChannelUris)
+                {
+                    channel.StopListening(s);
+                }
+
+                ChannelServices.UnregisterChannel(channel);
+            }
+
+            gameRoom = null;
+            Player = null;
+            channel = null;
+        }
+
+        // Find the next available port to use
+        int FindUnusedPort(IPAddress localAddr)
+        {
+            int retVal = 0;
+
+            for (int p = 8080; p <= IPEndPoint.MaxPort; p++)
+            {
+                Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    s.Bind(new IPEndPoint(localAddr, p));
+                    s.Close();
+
+                    retVal = p;
+
+                    break;
+                }
+                catch (SocketException)
+                {
+                    continue;
+                }
+            }
+
+            return retVal;
+        }
+    }
+}
